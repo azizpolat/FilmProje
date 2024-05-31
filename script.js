9c76cf97471c4d07cc28a1f4b6494888f892a211
Aziz <abdulazizpolat@gmail.com>
2024-06-01 00:26:49 +0300

    son

--- script.js
+++ script.js
@@ -2,17 +2,19 @@ const arrows = document.querySelectorAll(".arrow");
 const movieList = document.querySelectorAll(".movie-list");
 
 arrows.forEach((arrow, i) => {
+    const widtRadio = Math.floor(window.innerHeight /300)
   let clickCounter = 0;
   const imageCount = movieList[i].querySelectorAll("img").length;
    arrow.addEventListener("click", function () {
       clickCounter++;
-    if (imageCount - (4 + clickCounter) >= 0) {
+    if (imageCount - (4 + clickCounter) + (4-widtRadio) >= 0) {
       movieList[i].style.transform = `translateX(${
         movieList[i].computedStyleMap().get("transform")[0].x.value - 300
       }px)`;
    
   }else{
     movieList[i].style.transform = "translateX(0)"
+    clickCounter = 0;
   }
 });
 });
